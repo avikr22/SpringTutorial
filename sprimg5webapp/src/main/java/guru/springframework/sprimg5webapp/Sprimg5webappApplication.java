d54228cba188de3b5688806cde669086bba98097
Avinash <avi.kumar.2204@gmail.com>
2017-08-24 00:46:54 +0530

    First Commit

---
+++ sprimg5webapp/src/main/java/guru/springframework/sprimg5webapp/Sprimg5webappApplication.java
@@ -0,0 +1,12 @@
+package guru.springframework.sprimg5webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sprimg5webappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sprimg5webappApplication.class, args);
+	}
+}
